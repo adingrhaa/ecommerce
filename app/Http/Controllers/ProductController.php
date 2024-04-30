85b338e41e11863a4aabbbf03057399741f76805
unknown <nugrahacahyaadi@gmail.com>
2024-04-30 14:13:18 +0700

    memodifikasi kode pada file ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,75 +1,196 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
+ 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Storage; 
-
+use Psy\CodeCleaner\ReturnTypePass;
+ 
 class ProductController extends Controller
 {
-
+ 
     public function __construct(){
         $this->middleware('auth:api')->except(['index']);
     }
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
+ 
     {
         $products = Product::all();
+ 
         return response()->json([
             'data' => $products
         ]);
     }
-
+ 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+ 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'id_kategori' => 'required',
             'id_subkategori' => 'required',
             'nama_barang' => 'required',
+            'gambar' => 'required|image|mimes:jpg,png,webp',
+            'deskripsi' => 'required',
             'harga' => 'required',
             'diskon' => 'required',
             'bahan' => 'required',
             'tags' => 'required',
             'sku' => 'required',
             'ukuran' => 'required',
-            'warna' => 'required',
-            'deskripsi' => 'required',
-            'gambar' => 'required|image|mimes:jpg,png,webp'
+            'warna' => 'required'
         ]);
-
+ 
         if ($validator->fails()){
             return response()->json(
                 $validator->errors(),
                 422
             );
         };
-
+ 
         $input = $request->all();
-
+ 
         if ($request->has('gambar')){
             $gambar = $request->file('gambar');
             $nama_gambar = time() . rand(1,9) . '.' . $gambar->getClientOriginalExtension();
-            $gambar->storeAs('public/uploads', $nama_gambar); // Store the image in 'public/uploads' directory
+            $path = $gambar->storeAs('public/images', $nama_gambar);
             $input['gambar'] = $nama_gambar;
+   
+            $url_gambar = asset('storage/images/' . $nama_gambar);
+            $input['url_gambar'] = $url_gambar;
         }
-
+     
+ 
         $product = Product::create($input);
-
+ 
+        return response()->json([
+            'data' => $product
+        ]);
+       
+    }
+ 
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
         return response()->json([
             'data' => $product
         ]);
     }
-
-    public function show(Product $product)
+ 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $produk)
     {
-        // Generate public URL for image
-        $product->gambar = asset('storage/uploads/' . $product->gambar);
-        
+        //
+    }
+ 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validator = Validator::make($request->all(), [
+            'id_kategori' => 'required',
+            'id_subkategori' => 'required',
+            'nama_barang' => 'required',
+            'gambar' => 'required|image|mimes:jpg,png,webp',
+            'deskripsi' => 'required',
+            'harga' => 'required',
+            'diskon' => 'required',
+            'bahan' => 'required',
+            'tags' => 'required',
+            'sku' => 'required',
+            'ukuran' => 'required',
+            'warna' => 'required'
+        ]);
+   
+        if ($validator->fails()){
+            return response()->json(
+                $validator->errors(),
+                422
+            );
+        }
+   
+        $input = $request->all();
+   
+        if ($request->hasFile('gambar')) {
+            // Menghapus gambar yang sudah ada
+            if ($product->gambar) {
+                File::delete(public_path('storage/images/' . $product->gambar));
+            }
+   
+            // Mengunggah gambar yang baru
+            $gambar = $request->file('gambar');
+            $nama_gambar = time() . rand(1,9) . '.' . $gambar->getClientOriginalExtension();
+            $path = $gambar->storeAs('public/images', $nama_gambar);
+            $input['gambar'] = $nama_gambar;
+        } else {
+            // Jika tidak ada gambar baru, hapus informasi gambar dari input
+            unset($input['gambar']);
+        }
+   
+        // Memperbarui data kategori
+        $product->update($input);
+   
         return response()->json([
-            'data' => $product 
+            'message' => 'success',
+            'data' => $product
         ]);
     }
-
+   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+{
+    if ($product->gambar) {
+        // Hapus gambar terkait jika ada
+        File::delete(public_path('storage/images/' . $product->gambar));
+    }
+ 
+    // Hapus data kategori dari database
+    $product->delete();
+ 
+    return response()->json([
+        'message' => 'success'
+    ]);
+}
+ 
 }
