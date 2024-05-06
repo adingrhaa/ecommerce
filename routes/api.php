db33822a34079d903b4b85af296a44937a7d4750
unknown <nugrahacahyaadi@gmail.com>
2024-05-06 16:17:16 +0700

    memodifikasi routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,18 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\SubcategoryController;
-use App\Http\Controllers\SliderController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\MemberController;
-use App\Http\Controllers\TestimoniController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\SliderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TestimoniController;
+use App\Http\Controllers\SubcategoryController;
+use App\Http\Controllers\AuthenticationController;
 
 Route::group([
     'middleware' => 'api',
@@ -20,9 +21,17 @@ Route::group([
     Route::post('admin', [AuthController::class, 'login'])->name('login');
     Route::post('register', [AuthController::class, 'register'])->name('register');
 
-    Route::post('logout', [AuthController::class, 'logout']);
+    
+    // Route::post('logout', [AuthController::class, 'logout']);
     // Route::post('login', [AuthController::class, 'login_member']); //ketika menggunakan api {nyalakan}
 });
+
+Route::get('products', [ProductController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('products/{products}', [ProductController::class, 'show'])->middleware(['auth:sanctum']);
+
+Route::post('login', [AuthenticationController::class, 'login_member']);
+Route::get('logout', [AuthenticationController::class, 'logout_member'])->middleware(['auth:sanctum']);
+Route::get('me', [AuthenticationController::class, 'me'])->middleware(['auth:sanctum']);
 
 
 Route::group([
@@ -32,7 +41,7 @@ Route::group([
         'categories' => CategoryController::class,
         'subcategories' => SubcategoryController::class,
         'sliders' => SliderController::class,
-        'products' => ProductController::class,
+        // 'products' => ProductController::class,
         'members' => MemberController::class,
         'testimonis' => TestimoniController::class,
         'reviews' => ReviewController::class,
