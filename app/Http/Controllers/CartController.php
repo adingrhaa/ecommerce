0fc16eca63aa1f0ec82a204de1a5f77f3c3ffaa9
Cahya Adi Nugraha <nugrahacahyaadi@gmail.com>
2024-05-21 12:53:19 +0700

    feat: memodifikasi Cart-Checkout information controller dan routes/api.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,7 +11,7 @@ class CartController extends Controller
 {
 
     public function __construct(){
-        $this->middleware('auth:api')->except(['index']);
+        $this->middleware('auth:sanctum')->except(['index']);
     }
     /**
      * Display a listing of the resource.
